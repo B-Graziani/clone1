c4ae7c2416ddc0a1835e366a754c12c30383f53f
benoit graziani <benoit.c.graziani@hotmail.fr>
2021-05-05 10:34:15 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "cleaning DB"
+Flat.destroy_all
+puts "creating DB"
+flat1 = { title: "title 1", address: "address1", description: "description1description1description1description1", stars: "1"}
+flat2 = { title: "title 2", address: "address2", description: "description2description2description2description2", stars: "2"}
+flat3 = { title: "title 3", address: "address3", description: "description3description3description3description3", stars: "3"}
+flat4 = { title: "title 4", address: "address4", description: "description4description4description4description4", stars: "4"}
+flat5 = { title: "title 5", address: "address5", description: "description5description5description5description5", stars: "5"}
+Flats = []
+[flat1, flat2, flat3, flat4, flat5].each_with_index do |attributes, index|
+flat = Flat.create!(attributes)
+Flats << flat
+end
+
