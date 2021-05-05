720d8c23b9564fe55bd678df6ce7fd86715cd9bd
benoit graziani <benoit.c.graziani@hotmail.fr>
2021-05-05 11:40:22 +0200

    add email

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,4 +2,43 @@ class FlatsController < ApplicationController
   def index
     @flats = Flat.all
   end
+  def show
+    @flat = Flat.find(params[:id])
+  end
+  def top
+    @flats = Flat.where('stars >= 5')
+  end
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+  def update
+    @flat = Flat.find(params[:id])
+    if @flat.update(flat_params)
+      redirect_to @flat
+    else
+      render :edit
+    end
+  end
+  def new
+    @flat = Flat.new
+  end
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to @flat
+    else
+      render :new
+    end
+  end
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:title, :address, :description, :stars)
+  end
 end
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,2 +1,6 @@
 class Flat < ApplicationRecord
+  validates :title, presence: true
+  validates :address, presence: true
+  validates :description, presence: true
+  validates :stars, presence: true
 end
